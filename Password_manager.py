02561574d17f69dc8ab6463215aa57f59dd0fd9c
Prajwal2112 <thombreprajwal00000@gmail.com>
2025-09-03 17:41:26 +0530

    Update Password_manager.py

---
+++ Password_manager.py
@@ -0,0 +1,33 @@
+master_pwd = input("What is the master password?")
+
+def view():
+    with open("passwords.text", "r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("user:",user, ", password:", passw)
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open("password.txt", "a") as f:
+        f.write(name + " " + pwd + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit: ")
+    if mode == "q":
+        break
+
+    elif mode == "view":
+        view()
+
+    elif mode == "add":
+        add()
+
+    else:
+        print("Invalid mode.")
+        continue
+
+
+
